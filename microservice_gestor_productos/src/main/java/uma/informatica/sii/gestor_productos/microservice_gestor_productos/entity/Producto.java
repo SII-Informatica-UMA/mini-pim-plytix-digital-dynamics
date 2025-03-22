b3526aedcacd6c171548c6a9fe503583ec8e977f
Miguel Ángel <152654777+Miguel-Angel-04@users.noreply.github.com>
2025-03-22 17:06:21 +0100

    Update Producto.java
    Implementacion Entidad Producto

--- microservice_gestor_productos/src/main/java/uma/informatica/sii/gestor_productos/microservice_gestor_productos/entity/Producto.java
+++ microservice_gestor_productos/src/main/java/uma/informatica/sii/gestor_productos/microservice_gestor_productos/entity/Producto.java
@@ -1,67 +1,115 @@
 package uma.informatica.sii.gestor_productos.microservice_gestor_productos.entity;
 
-import javax.persistence.*;
-import java.util.Objects;
+import java.util.*;
+import jakarta.persistence.*;
+import java.io.*;
+import java.time.LocalDateTime;
 
 @Entity
-public class Producto {
-
+@Table( uniqueConstraints = {@UniqueConstraint(columnNames = {"gtin"})})
+public class Producto implements Serializable {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(nullable = false, unique = true) // GTIN único y obligatorio
+    private String gtin;
+    @Column(nullable = false)
+    private String sku;
+    @Column(nullable = false)
     private String nombre;
-    private Double precio;
+    @Column(length = 255)
+    private String textoCorto;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime creado;
+    private LocalDateTime modificado;
+    private String miniatura;
 
-    @ManyToOne
-    @JoinColumn(name = "categoria_id")
-    private Categoria categoria;
-
-    // Getters y Setters
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(Double precio) {
-        this.precio = precio;
-    }
-
-    public Categoria getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(Categoria categoria) {
-        this.categoria = categoria;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Producto)) return false;
-        Producto producto = (Producto) o;
-        return Objects.equals(id, producto.id);
-    }
-
+    // Implementación de hashCode y equals basada en GTIN
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(gtin);
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Producto producto = (Producto) obj;
+        return Objects.equals(gtin, producto.gtin);
+    }
+
+    // Implementación de toString
     @Override
     public String toString() {
         return "Producto{" +
                 "id=" + id +
+                ", gtin='" + gtin + '\'' +
+                ", sku='" + sku + '\'' +
                 ", nombre='" + nombre + '\'' +
-                ", precio=" + precio +
-                ", categoria=" + categoria +
+                ", textoCorto='" + textoCorto + '\'' +
+                ", creado=" + creado +
+                ", modificado=" + modificado +
+                ", miniatura='" + miniatura + '\'' +
                 '}';
     }
-} 
+
+    @Collumn(nullable = false)
+    private Long cuentaId;
+
+    @ManyToMany
+    @JoinTable(
+        name = "producto_categoria",
+        joinColumns = @JoinColumn(name = "producto_id"),
+        inverseJoinColumns = @JoinColumn(name = "categoria_id")
+    )
+    private Set<CategoriaProducto> categorias = new HashSet<>();
+
+    @OneToMany(mappedBy = "productoOrigen")
+    private Set<RelacionProducto> relacionesOrigen = new HashSet<>();
+
+    @OneToMany(mappedBy = "productoDestino")
+    private Set<RelacionProducto> relacionesDestino = new HashSet<>();
+
+    public Producto() {
+        this.creado = LocalDateTime.now();
+        this.modificado = LocalDateTime.now();
+    }
+
+
+    // Getters y Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getGtin() { return gtin; }
+    public void setGtin(String gtin) { this.gtin = gtin; }
+
+    public String getSku() { return sku; }
+    public void setSku(String sku) { this.sku = sku; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getTextoCorto() { return textoCorto; }
+    public void setTextoCorto(String textoCorto) { this.textoCorto = textoCorto; }
+
+    public LocalDateTime getCreado() { return creado; }
+
+    public LocalDateTime getModificado() { return modificado; }
+    public void setModificado(LocalDateTime modificado) { this.modificado = modificado; }
+
+    public String getMiniatura() { return miniatura; }
+    public void setMiniatura(String miniatura) { this.miniatura = miniatura; }
+
+    public Long getCuentaId() { return cuentaId; }
+    public void setCuentaId(Long cuentaId) { this.cuentaId = cuentaId; }
+
+    public Set<CategoriaProducto> getCategorias() { return categorias; }
+    public void setCategorias(Set<CategoriaProducto> categorias) { this.categorias = categorias; }
+
+    public Set<RelacionProducto> getRelacionesOrigen() { return relacionesOrigen; }
+    public void setRelacionesOrigen(Set<RelacionProducto> relacionesOrigen) { this.relacionesOrigen = relacionesOrigen; }
+
+    public Set<RelacionProducto> getRelacionesDestino() { return relacionesDestino; }
+    public void setRelacionesDestino(Set<RelacionProducto> relacionesDestino) { this.relacionesDestino = relacionesDestino; }
+}
