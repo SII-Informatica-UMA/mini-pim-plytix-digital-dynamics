1e53378d7d3f85eb27fa7ccb16141244503875da
Miguel Ángel <152654777+Miguel-Angel-04@users.noreply.github.com>
2025-03-22 18:22:07 +0100

    Update CategoriaRepository.java

--- microservice_gestor_productos/src/main/java/uma/informatica/sii/gestor_productos/microservice_gestor_productos/repository/CategoriaRepository.java
+++ microservice_gestor_productos/src/main/java/uma/informatica/sii/gestor_productos/microservice_gestor_productos/repository/CategoriaRepository.java
@@ -4,4 +4,11 @@ import uma.informatica.sii.gestor_productos.microservice_gestor_productos.entity
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+  // Buscar categoría por ID
+    @Query("SELECT c FROM CategoriaProducto c WHERE c.id = :id")
+    List<CategoriaProducto> findById(Long id);
+
+    // Buscar categorías por nombre
+    @Query("SELECT c FROM CategoriaProducto c WHERE c.nombre = :nombre")
+    List<CategoriaProducto> findByNombre(String nombre);
 }
