f1aa2a5dd7920c642f5c64485d196e636953c767
edu1612 <edunosto@uma.es>
2025-03-22 16:14:19 +0100

    Proyecto Spring Boot creado y corregido con estructura completa(vacio)

---
+++ microservice_gestor_productos/src/main/java/uma/informatica/sii/gestor_productos/microservice_gestor_productos/MicroserviceGestorProductosApplication.java
@@ -0,0 +1,13 @@
+package uma.informatica.sii.gestor_productos.microservice_gestor_productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGestorProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceGestorProductosApplication.class, args);
+	}
+
+}
---
+++ microservice_gestor_productos/src/test/java/uma/informatica/sii/gestor_productos/microservice_gestor_productos/MicroserviceGestorProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package uma.informatica.sii.gestor_productos.microservice_gestor_productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceGestorProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
