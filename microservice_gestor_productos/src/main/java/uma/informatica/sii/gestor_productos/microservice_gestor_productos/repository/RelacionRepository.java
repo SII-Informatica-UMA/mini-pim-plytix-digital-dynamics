a699959b9d9cc9fb504abd493261a4c0a525031d
iagugar <ivanaguilargarcia@uma.es>
2025-03-22 18:13:53 +0100

    relacion repository con metodos

---
+++ microservice_gestor_productos/src/main/java/uma/informatica/sii/gestor_productos/microservice_gestor_productos/repository/RelacionRepository.java
@@ -0,0 +1,19 @@
+package uma.informatica.sii.gestor_productos.microservice_gestor_productos.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import uma.informatica.sii.gestor_productos.microservice_gestor_productos.entity.Relacion;
+
+public interface RelacionRepository extends JpaRepository<Relacion, Long> {
+	@Query("SELECT r FROM Relaciones r WHERE r.id = :id")
+	List<Relacion> findById(Integer id);
+	@Query("INSERT INTO Relaciones r (r.nombre, r.descripcion) VALUES (:nombre, :descripcion)")
+	Relacion insert(Relacion r);
+	@Query("UPDATE Relaciones r SET r.nombre = :nombre, r.descripcion = :descripcion WHERE r.id = :id")
+	Relacion modifyById(Relacion r);
+	@Query("DELETE FROM Relaciones r WHERE r.id = :id")
+	void deleteById(int id);
+}
