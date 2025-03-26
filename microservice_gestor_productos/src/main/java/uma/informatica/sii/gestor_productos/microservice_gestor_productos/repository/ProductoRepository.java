f0e77893991580f35c57d2b31d4c4009292bd9d0
iagugar <ivanaguilargarcia@uma.es>
2025-03-26 13:07:25 +0100

    Repositorio producto actualizado, metodo para buscar por cuenta

--- microservice_gestor_productos/src/main/java/uma/informatica/sii/gestor_productos/microservice_gestor_productos/repository/ProductoRepository.java
+++ microservice_gestor_productos/src/main/java/uma/informatica/sii/gestor_productos/microservice_gestor_productos/repository/ProductoRepository.java
@@ -2,22 +2,28 @@ package uma.informatica.sii.gestor_productos.microservice_gestor_productos.repos
 
 import uma.informatica.sii.gestor_productos.microservice_gestor_productos.entity.Producto;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    // Obtener producto por id
+    //Obtener Producto por id
     @Query("SELECT p FROM Producto p WHERE p.id = :id")
-	List<Producto> findById(Integer id);
-    // Crear un producto
-    @Query("INSERT INTO Producto p (p.id,p.gtin,p.sku,p.nombre,p.textoCorto,p.creado,p.modificado,p.miniatura,p.categorias,p.relacionesOrigen,p.relacionesDestino) VALUES (:id,:gtin,:sku,:nombre,:textoCorto,:creado,:modificado,:miniatura,:categorias,:relacionesOrigen,:relacionesDestino)")
-	Producto insert(Producto r);
-    // Modificar un producto
-    @Query("UPDATE Producto p SET p.gtin = :gtin, p.sku = :sku, p.nombre = :nombre, p.textoCorto = :textoCorto, p.creado = :creado, p.modificado = :modificado, p.miniatura = :miniatura, p.categorias = :categorias, p.relacionesOrigen = :relacionesOrigen, p.relacionesDestino = :relacionesDestino WHERE p.id = :id")
-    Producto update(Producto r);
-    // Eliminar un producto
-	@Query("DELETE FROM Producto p WHERE p.id = :id")
-	void deleteById(Integer id);
+    List<Producto> findById(Integer id);
 
+	//Obtener Producto por cuenta
+    @Query("SELECT p FROM Producto p WHERE p.cuentaId = :cuentaId")
+    Producto findByCuentaId(Integer cuentaId);
+
+	//Eliminar Producto por id
+    @Query("DELETE FROM Producto p WHERE p.id = :id")
+    Producto deleteById(Integer id);
+
+	//Insertar Producto
+    @Query("INSERT INTO Producto p (p.id,p.gtin,p.sku,p.nombre,p.textoCorto,p.creado,p.modificado,p.miniatura,p.categorias,p.relacionesOrigen,p.relacionesDestino,p.cuentaId) VALUES (:id,:gtin,:sku,:nombre,:textoCorto,:creado,:modificado,:miniatura,:categorias,:relacionesOrigen,:relacionesDestino,:cuentaId)")
+	Producto insert(Producto r);
+
+	//Modificar Producto
+    @Query("UPDATE Producto p SET p.gtin = :gtin, p.sku = :sku, p.nombre = :nombre, p.textoCorto = :textoCorto, p.creado = :creado, p.modificado = :modificado, p.miniatura = :miniatura, p.categorias = :categorias, p.relacionesOrigen = :relacionesOrigen, p.relacionesDestino = :relacionesDestino, p.cuentaId = :cuentaId WHERE p.id = :id")
+    Producto update(Producto r);
 } 
