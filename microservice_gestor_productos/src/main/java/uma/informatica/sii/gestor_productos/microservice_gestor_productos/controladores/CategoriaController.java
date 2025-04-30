0c867d402351d940545369b4c81454233929b943
Youssef Merroun <youssefmerroun333@gmail.com>
2025-04-30 17:36:11 +0200

    algunas correciones

--- microservice_gestor_productos/src/main/java/uma/informatica/sii/gestor_productos/microservice_gestor_productos/controladores/CategoriaController.java
+++ microservice_gestor_productos/src/main/java/uma/informatica/sii/gestor_productos/microservice_gestor_productos/controladores/CategoriaController.java
@@ -5,54 +5,63 @@ import uma.informatica.sii.gestor_productos.microservice_gestor_productos.servic
 import uma.informatica.sii.gestor_productos.microservice_gestor_productos.excepciones.EntidadNoExistente;
 import uma.informatica.sii.gestor_productos.microservice_gestor_productos.excepciones.SinPermisosSuficientes;
 import uma.informatica.sii.gestor_productos.microservice_gestor_productos.excepciones.CredencialesNoValidas;
+import uma.informatica.sii.gestor_productos.microservice_gestor_productos.mappers.CategoriaMapper;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.yaml.snakeyaml.events.Event.ID;
+
+import jakarta.persistence.IdClass;
 
 import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categoria")
+@RequestMapping("/categoria-producto")
 public class CategoriaController {
 
     private final CategoriaService categoriaService;
-
-    public CategoriaController(CategoriaService categoriaService) {
+    private final CategoriaMapper categoriaMapper;
+    public CategoriaController(CategoriaService categoriaService, CategoriaMapper categoriaMapper) {
         this.categoriaService = categoriaService;
+        this.categoriaMapper = categoriaMapper;
     }
 
     @GetMapping
     public ResponseEntity<?> getCategoria(
             @RequestParam(required = false) Integer idCategoria,
-            @RequestParam(required = false) Integer cuentaId,
-            @RequestHeader("Authorization") String jwtToken) {
-
+            @RequestParam(required = false) Integer idCuenta,
+            @RequestHeader("Authorization") String authorizationHeader) {
+        
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
         int count = 0;
         if (idCategoria != null) count++;
-        if (cuentaId != null) count++;
+        if (idCuenta != null) count++;
 
         if (count != 1) {
-            return ResponseEntity.badRequest().body("Debe proporcionar exactamente un parámetro: idCategoria o cuentaId.");
+            return ResponseEntity.badRequest().body("Debe proporcionar exactamente un parámetro: idCategoria o idCuenta.");
         }
 
         if (idCategoria != null) {
             return ResponseEntity.ok(categoriaService.getCategoriaById(idCategoria, jwtToken));
         }
 
-        return ResponseEntity.ok(categoriaService.getCategoriasByCuentaId(cuentaId, jwtToken));
+        return ResponseEntity.ok(categoriaService.getCategoriasByidCuenta(idCuenta, jwtToken));
     }
 
     @PostMapping
     public ResponseEntity<CategoriaDTO> crearCategoria(
             @RequestBody CategoriaDTO dto,
-            @RequestHeader("Authorization") String jwtToken,
+            @RequestParam Integer idCuenta,
+            @RequestHeader("Authorization") String authorizationHeader,
             UriComponentsBuilder builder) {
+        
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
 
-        CategoriaDTO nueva = categoriaService.crearCategoria(dto, jwtToken);
-        URI uri = builder.path("/categoria/{id}").buildAndExpand(nueva.getId()).toUri();
+        CategoriaDTO nueva = categoriaService.crearCategoria(dto, idCuenta, jwtToken);
+        URI uri = builder.path("/{id}").buildAndExpand(nueva.getId()).toUri();
         return ResponseEntity.created(uri).body(nueva);
     }
 
@@ -60,15 +69,19 @@ public class CategoriaController {
     public ResponseEntity<CategoriaDTO> modificarCategoria(
             @PathVariable Integer idCategoria,
             @RequestBody CategoriaDTO dto,
-            @RequestHeader("Authorization") String jwtToken) {
+            @RequestHeader("Authorization") String authorizationHeader) {
 
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
+        
         return ResponseEntity.ok(categoriaService.modificarCategoria(idCategoria, dto, jwtToken));
     }
 
     @DeleteMapping("/{idCategoria}")
     public ResponseEntity<Void> eliminarCategoria(
             @PathVariable Integer idCategoria,
-            @RequestHeader("Authorization") String jwtToken) {
+            @RequestHeader("Authorization") String authorizationHeader) {
+
+        String jwtToken = authorizationHeader.replace("Bearer ", "");
 
         categoriaService.eliminarCategoria(idCategoria, jwtToken);
         return ResponseEntity.noContent().build();
--- microservice_gestor_productos/src/main/java/uma/informatica/sii/gestor_productos/microservice_gestor_productos/servicios/CategoriaService.java
+++ microservice_gestor_productos/src/main/java/uma/informatica/sii/gestor_productos/microservice_gestor_productos/servicios/CategoriaService.java
@@ -3,11 +3,13 @@ package uma.informatica.sii.gestor_productos.microservice_gestor_productos.servi
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import uma.informatica.sii.gestor_productos.microservice_gestor_productos.Usuario.Usuario;
 import uma.informatica.sii.gestor_productos.microservice_gestor_productos.Usuario.UsuarioDTO;
 import uma.informatica.sii.gestor_productos.microservice_gestor_productos.Usuario.UsuarioService;
 import uma.informatica.sii.gestor_productos.microservice_gestor_productos.dtos.CategoriaDTO;
 import uma.informatica.sii.gestor_productos.microservice_gestor_productos.entity.Categoria;
 import uma.informatica.sii.gestor_productos.microservice_gestor_productos.excepciones.EntidadNoExistente;
+import uma.informatica.sii.gestor_productos.microservice_gestor_productos.excepciones.SinPermisosSuficientes;
 import uma.informatica.sii.gestor_productos.microservice_gestor_productos.mappers.CategoriaMapper;
 import uma.informatica.sii.gestor_productos.microservice_gestor_productos.repository.CategoriaRepository;
 
@@ -32,58 +34,65 @@ public class CategoriaService {
 
         Long idUsuario = usuarioService.getUsuarioConectado(jwtToken)
                 .map(UsuarioDTO::getId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Usuario no autenticado"));
+                .orElseThrow(() -> new SinPermisosSuficientes());
 
-        if (!usuarioService.usuarioPerteneceACuenta(categoria.getCuentaId(), idUsuario, jwtToken)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Acceso denegado: no puedes ver esta categoría.");
+        if (!usuarioService.usuarioPerteneceACuenta(categoria.getId(), idUsuario, jwtToken)) {
+            throw new SinPermisosSuficientes();
         }
 
         return CategoriaMapper.toDTO(categoria);
     }
 
-    public List<CategoriaDTO> getCategoriasByCuentaId(Integer cuentaId, String jwtToken) {
+    public List<CategoriaDTO> getCategoriasByidCuenta(Integer idCuenta, String jwtToken) {
         Long idUsuario = usuarioService.getUsuarioConectado(jwtToken)
                 .map(UsuarioDTO::getId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Usuario no autenticado"));
+                .orElseThrow(() -> new SinPermisosSuficientes());
 
-        if (!usuarioService.usuarioPerteneceACuenta(cuentaId, idUsuario, jwtToken)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Acceso denegado: no puedes ver las categorías de esta cuenta.");
+        UsuarioDTO usuario = usuarioService.getUsuarioConectado(jwtToken)
+                .orElseThrow(() -> new SinPermisosSuficientes());
+
+        if (!usuario.getRole().equals(Usuario.Rol.ADMINISTRADOR)) {
+            boolean pertenece = usuarioService.usuarioPerteneceACuenta(idCuenta, usuario.getId(), jwtToken);
+            if (!pertenece) {
+                throw new SinPermisosSuficientes();
+            }
         }
 
         return categoriaRepository.findAll().stream()
-                .filter(cat -> cat.getCuentaId().equals(cuentaId))
+                .filter(cat -> cat.getId().equals(idCuenta))
                 .map(CategoriaMapper::toDTO)
                 .collect(Collectors.toList());
     }
 
-    public CategoriaDTO crearCategoria(CategoriaDTO dto, String jwtToken) {
+    public CategoriaDTO crearCategoria(CategoriaDTO dto, Integer idCuenta, String jwtToken) {
         Optional<Categoria> existente = categoriaRepository.findByNombre(dto.getNombre());
         if (existente.isPresent()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "La categoría '" + dto.getNombre() + "' ya existe.");
+            throw new EntidadNoExistente();
         }
 
         Categoria nueva = CategoriaMapper.toEntity(dto);
 
         Long idUsuario = usuarioService.getUsuarioConectado(jwtToken)
                 .map(UsuarioDTO::getId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Usuario no autenticado"));
+                .orElseThrow(() -> new SinPermisosSuficientes());
 
-        Integer cuentaId = usuarioService.usuarioPerteneceACuenta(dto.getId(), idUsuario, jwtToken) ? dto.getId() : null;
-        nueva.setCuentaId(cuentaId);
+        idCuenta = usuarioService.usuarioPerteneceACuenta(dto.getId(), idUsuario, jwtToken) ? dto.getId() : null;
+        nueva.setId(idCuenta);
 
         Categoria guardada = categoriaRepository.save(nueva);
         return CategoriaMapper.toDTO(guardada);
     }
+
     public CategoriaDTO modificarCategoria(Integer idCategoria, CategoriaDTO dto, String jwtToken) {
         Categoria categoria = categoriaRepository.findById(idCategoria)
                 .orElseThrow(EntidadNoExistente::new);
 
         Long idUsuario = usuarioService.getUsuarioConectado(jwtToken)
                 .map(UsuarioDTO::getId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Usuario no autenticado"));
+                .orElseThrow(() -> new SinPermisosSuficientes());
 
-        if (!usuarioService.usuarioPerteneceACuenta(categoria.getCuentaId(), idUsuario, jwtToken)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Acceso denegado: no puedes modificar esta categoría.");
+        if (!usuarioService.usuarioPerteneceACuenta(categoria.getId(), idUsuario, jwtToken)) {
+            throw new SinPermisosSuficientes();
         }
 
         categoria.setNombre(dto.getNombre());
@@ -98,10 +107,10 @@ public class CategoriaService {
 
         Long idUsuario = usuarioService.getUsuarioConectado(jwtToken)
                 .map(UsuarioDTO::getId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Usuario no autenticado"));
+                .orElseThrow(() -> new SinPermisosSuficientes());
 
-        if (!usuarioService.usuarioPerteneceACuenta(categoria.getCuentaId(), idUsuario, jwtToken)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Acceso denegado: no puedes eliminar esta categoría.");
+        if (!usuarioService.usuarioPerteneceACuenta(categoria.getId(), idUsuario, jwtToken)) {
+            throw new EntidadNoExistente();
         }
 
         categoriaRepository.delete(categoria);
